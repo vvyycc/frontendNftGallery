46af4ac483406c36ac637f8bce4b0e3776f1fa01
Víctor Roldan Quesada <victorroldan18@gmail.com>
2023-06-28 15:07:27 +0000

    Principal page of gallery

--- nft-album-gallery/src/App.js
+++ nft-album-gallery/src/App.js
@@ -1,25 +1,187 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Web3 from 'web3';
 
-function App() {
+
+
+
+function Copyright() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
-export default App;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function App() {
+  const [isWalletConnected, setWalletConnected] = useState(false);
+  const [connectedAccount, setConnectedAccount] = useState('');
+
+  async function connectWallet() {
+
+    // Check if MetaMask is installed
+    if (window.ethereum) {
+      try {
+        // Request access to the user's MetaMask accounts
+        await window.ethereum.enable();
+
+        // Create a Web3 instance
+        const web3 = new Web3(window.ethereum);
+
+        // You can now use the `web3` instance to interact with the blockchain
+
+        // Example: Get the connected account
+        const accounts = await web3.eth.getAccounts();
+        const connectedAccount = accounts[0];
+
+        await setWalletConnected(true);
+        await setConnectedAccount(connectedAccount);
+        console.log('Connected account:', connectedAccount);
+      } catch (error) {
+        console.error('Error connecting with MetaMask:', error);
+      }
+    } else {
+      console.error('MetaMask not detected. Please install MetaMask to connect your wallet.');
+    }
+  }
+
+  function handleConnectWallet() {
+    if (isWalletConnected) {
+      setWalletConnected(false);
+      setConnectedAccount('');
+    } else {
+      connectWallet();
+    }
+  }
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          {/* <CameraIcon sx={{ mr: 2 }} /> */}
+          <Typography variant="h6" color="inherit" noWrap>
+            <Button variant="contained" onClick={handleConnectWallet} style={{ backgroundColor: isWalletConnected ? 'green' : 'inherit' }}>
+              {isWalletConnected ? 'Connected' : 'Connect Wallet'}
+            </Button>
+          </Typography>
+          {isWalletConnected && (
+            <Typography variant="body1" style={{ color: 'white', marginLeft: 'auto' }}>
+              Connected Account: {connectedAccount}
+            </Typography>
+          )}
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Album layout
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              Something short and leading about the collection below—its contents,
+              the creator, etc. Make it short and sweet, but not too short so folks
+              don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
